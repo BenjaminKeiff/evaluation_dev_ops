dd94605dacf0c6149954c4a6e22dd8d1d0acb957
Benjamin KEIFF <benjamin.keiff@stagiairesmns.fr>
2024-02-21 17:04:15 +0100

    rendu

--- TestEvaluation.UnitTests/BaptizmTests.cs
+++ TestEvaluation.UnitTests/BaptizmTests.cs
@@ -1,11 +1,19 @@
+using EvaluationSampleCode;
+
 namespace TestEvaluation.UnitTests
 {
     [TestClass]
     public class BaptizmTests
     {
+        ClergyMember pope;
+        ClergyMember priest;
+
         [TestMethod]
-        public void TestMethod1()
+        public void CanBeBaptizedBy_IfIsClergyMember_ReturnTrue(Baptizm baptizm)
         {
+            var member = new ClergyMember();
+
+            Assert.IsTrue(baptizm.CanBeBaptizedBy(member));
         }
     }
 }
--- TestEvaluation.UnitTests/HtmlFormatHelperTests.cs
+++ TestEvaluation.UnitTests/HtmlFormatHelperTests.cs
@@ -1,11 +1,16 @@
+using EvaluationSampleCode;
+
 namespace TestEvaluation.UnitTests
 {
     [TestClass]
     public class HtmlFormatHelperTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetStrongFormat_IfIsStrong_ReturnTrue(HtmlFormatHelper htmlFormatHelper)
         {
+            var html = htmlFormatHelper.GetStrongFormat("bcsdbcvjsd");
+
+            Assert.IsNotNull(html);
         }
     }
 }
